f20f21ee0d3f9175e3d8927c1403c87b6cc66715
MONIDEPA GHOSH <123240415+monidepag@users.noreply.github.com>
2023-03-05 10:59:22 +0530

    Create SpringbootProjectApplication.java

---
+++ SpringbootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// configuration, component scan, Enable auto configuration
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+	}
+
+}
